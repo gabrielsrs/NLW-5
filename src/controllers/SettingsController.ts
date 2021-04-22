a92ddb425c8e3a899509cd47656c492edda6e508
gabrielsrs <55472396+gabrielsrs@users.noreply.github.com>
2021-04-22 02:51:58 -0300

    Final do dia 3 NLW5

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,22 +1,19 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "../repositories/SettingsRepository";
-
+import { SettingsServices } from "../services/SettingsServices"
 
 class SettingsController {
  async create(request: Request, response: Response) {
-   const { chat, username } = request.body;
-  
-   const settingsRepository = getCustomRepository(SettingsRepository);
-  
-   const settings = settingsRepository.create({
-    chat,
-    username,
-   });
-   
-   await settingsRepository.save(settings);
-  
+  const { chat, username } = request.body;
+
+  const settingsServices = new SettingsServices();
+
+  try{
+    const settings = await settingsServices.create({ chat, username });
+
    return response.json(settings);
+  }catch(err) {
+    return response.status(400).json({ message: err.message})
+  }
  }
 }
 
---
+++ src/services/UsersService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UsersService {
+ private usersRepository: Repository<User>;
+
+ constructor() {
+  this.usersRepository = getCustomRepository(UsersRepository);
+ }
+
+ async create(email: string) {
+
+  // Verificar se usuário existe 
+  const userAlreadyExist = await this.usersRepository.findOne({
+   email,
+  })
+
+  // Se não existir, salvar no DB
+  if (userAlreadyExist) {
+   return userAlreadyExist;
+  }
+
+  const user = this.usersRepository.create({
+  email,
+  })
+
+  await this.usersRepository.save(user);
+
+  // Se existir, retornar usuário
+  return user;
+ }
+}
+
+export { UsersService };
